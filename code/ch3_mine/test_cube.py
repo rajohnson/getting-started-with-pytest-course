d16897f491f4e1e78cd6be9b1a3961e60c6836b6
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-27 12:48:17 -0800

    use fixture

--- code/ch3_mine/test_cube.py
+++ code/ch3_mine/test_cube.py
@@ -1,6 +1,13 @@
-def test_cube():
+import pytest
+
+
+@pytest.fixture()
+def num():
+    return 42
+
+
+def test_cube(num):
     # given - number
-    num = 42
     # when - it is cubed
     cube = num**3
     # assert - cube is equal to n*n*n
