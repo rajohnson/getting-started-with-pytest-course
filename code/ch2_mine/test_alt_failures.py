940e184cc90e79113edee2b32d74e862af9f663d
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-26 19:03:02 -0800

    alternate failure mechanisms

---
+++ code/ch2_mine/test_alt_failures.py
@@ -0,0 +1,21 @@
+from ast import main
+import pytest
+from cards import Card
+
+
+def test_equality():
+    c1 = Card("asdf", "Rob", "todo", 5)
+    c2 = Card("asdf", 3, "todo", 5)
+    if c1 != c2:
+        pytest.fail("cards are not equal")
+
+
+def test_exception():
+    c1 = Card("asdf", "Rob", "todo", 5)
+    c2 = Card("asdf", 3, "todo", 5)
+    if c1 != c2:
+        raise Exception("cards are not equal")
+
+
+if __name__ == "__main__":
+    test_equality()
