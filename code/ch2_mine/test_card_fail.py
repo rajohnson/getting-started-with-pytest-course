852cdcd98e0314df9b51a8b992b1f0962ce2f24e
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-26 18:55:44 -0800

    tracebacks

---
+++ code/ch2_mine/test_card_fail.py
@@ -0,0 +1,11 @@
+from cards import Card
+
+
+def test_equality():
+    c1 = Card("asdf", "Rob", "todo", 5)
+    c2 = Card("asdf", 3, "todo", 5)
+    assert c1 == c2
+
+
+if __name__ == "__main__":
+    test_equality()
