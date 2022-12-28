b2a34eba94e8aa9bf3c5a855c196961db82dc70f
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-27 19:51:04 -0800

    additional test with one item pre-fixture

--- code/ch3_mine/test_card.py
+++ code/ch3_mine/test_card.py
@@ -3,7 +3,7 @@ from tempfile import TemporaryDirectory
 import cards
 
 
-def test_empty():
+def test_count_empty():
     # given - the database is empty
     with TemporaryDirectory() as temp_dir:
         db_path = pathlib.Path(temp_dir)
@@ -17,3 +17,20 @@ def test_empty():
 
         # assert - count is  0
         assert 0 == count
+
+
+def test_count_one_item():
+    # given - the database has one entry
+    with TemporaryDirectory() as temp_dir:
+        db_path = pathlib.Path(temp_dir)
+        db = cards.CardsDB(db_path)
+        db.add_card(cards.Card("task name"))
+
+        # when - count is called
+        count = db.count()
+
+        # teardown
+        db.close()
+
+        # assert - count is  1
+        assert 1 == count
