752665fce2570a0ec169ad776e05340462531658
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-28 17:03:48 -0800

    nested parametrization

--- code/ch4_mine/test_finish.py
+++ code/ch4_mine/test_finish.py
@@ -2,9 +2,10 @@ import cards
 import pytest
 
 
+@pytest.mark.parametrize("summary", ["asdf", "qwer", "zxcv"])
 @pytest.mark.parametrize("initial_state", ["todo", "in prog", "done"])
-def test_finish(cards_db, initial_state):
-    c = cards.Card("asdlafgl", state=initial_state)
+def test_finish(cards_db, initial_state, summary):
+    c = cards.Card(summary, state=initial_state)
     idx = cards_db.add_card(c)
     cards_db.finish(idx)
     card = cards_db.get_card(idx)
