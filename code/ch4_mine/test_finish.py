374b73924d9ede832a7affc771f787c4f58a4e9e
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-28 16:55:09 -0800

    use parametrization to test multiple cases

--- code/ch4_mine/test_finish.py
+++ code/ch4_mine/test_finish.py
@@ -1,11 +1,12 @@
 import cards
+import pytest
 
 
-def test_finish(cards_db):
-    for initial_state in ["todo", "in progress", "done"]:
-        c = cards.Card("asdlafgl", state=initial_state)
-        idx = cards_db.add_card(c)
-        cards_db.finish(idx)
-        card = cards_db.get_card(idx)
-        state = card.state
-        assert state == "done"
+@pytest.mark.parametrize("initial_state", ["todo", "in progress", "done"])
+def test_finish(cards_db, initial_state):
+    c = cards.Card("asdlafgl", state=initial_state)
+    idx = cards_db.add_card(c)
+    cards_db.finish(idx)
+    card = cards_db.get_card(idx)
+    state = card.state
+    assert state == "done"
