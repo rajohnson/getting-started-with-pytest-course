e2238a2e6b8206f0375875dac62ca7f156c5cf62
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-28 16:43:02 -0800

    test finish with all states

--- code/ch4_mine/test_finish.py
+++ code/ch4_mine/test_finish.py
@@ -1,8 +1,26 @@
 import cards
 
 
-def test_finish(cards_db):
+def test_finish_in_progress(cards_db):
     c = cards.Card("asdlafgl", state="in progress")
+    idx = cards_db.add_card(c)
+    cards_db.finish(idx)
+    card = cards_db.get_card(idx)
+    state = card.state
+    assert state == "done"
+
+
+def test_finish_todo(cards_db):
+    c = cards.Card("asdlafgl", state="todo")
+    idx = cards_db.add_card(c)
+    cards_db.finish(idx)
+    card = cards_db.get_card(idx)
+    state = card.state
+    assert state == "done"
+
+
+def test_finish_done(cards_db):
+    c = cards.Card("asdlafgl", state="done")
     idx = cards_db.add_card(c)
     cards_db.finish(idx)
     card = cards_db.get_card(idx)
