dc354201b328625ba785ac37030ae2e511557b63
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-28 17:10:29 -0800

    fixture parametrization

--- code/ch4_mine/test_finish.py
+++ code/ch4_mine/test_finish.py
@@ -2,8 +2,12 @@ import cards
 import pytest
 
 
+@pytest.fixture(params=["todo", "in prog", "done"])
+def initial_state(request):
+    return request.param
+
+
 @pytest.mark.parametrize("summary", ["asdf", "qwer", "zxcv"])
-@pytest.mark.parametrize("initial_state", ["todo", "in prog", "done"])
 def test_finish(cards_db, initial_state, summary):
     c = cards.Card(summary, state=initial_state)
     idx = cards_db.add_card(c)
