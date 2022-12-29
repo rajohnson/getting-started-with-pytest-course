b8d90dcd87cbd09ae1af3cf4982eac2eb5416e46
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-28 16:47:49 -0800

    collapse similar tests naively won't hit later tests if an early test fails.

--- code/ch4_mine/test_finish.py
+++ code/ch4_mine/test_finish.py
@@ -1,28 +1,11 @@
 import cards
 
 
-def test_finish_in_progress(cards_db):
-    c = cards.Card("asdlafgl", state="in progress")
-    idx = cards_db.add_card(c)
-    cards_db.finish(idx)
-    card = cards_db.get_card(idx)
-    state = card.state
-    assert state == "done"
-
-
-def test_finish_todo(cards_db):
-    c = cards.Card("asdlafgl", state="todo")
-    idx = cards_db.add_card(c)
-    cards_db.finish(idx)
-    card = cards_db.get_card(idx)
-    state = card.state
-    assert state == "done"
-
-
-def test_finish_done(cards_db):
-    c = cards.Card("asdlafgl", state="done")
-    idx = cards_db.add_card(c)
-    cards_db.finish(idx)
-    card = cards_db.get_card(idx)
-    state = card.state
-    assert state == "done"
+def test_finish(cards_db):
+    for initial_state in ["todo", "in progress", "done"]:
+        c = cards.Card("asdlafgl", state=initial_state)
+        idx = cards_db.add_card(c)
+        cards_db.finish(idx)
+        card = cards_db.get_card(idx)
+        state = card.state
+        assert state == "done"
